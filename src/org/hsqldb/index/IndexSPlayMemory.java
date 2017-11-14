11672b87ff0a262c5143c0c8508fbd134f368d3e
Chanakar <bmcrathnayaka@gmail.com>
2017-11-14 13:46:21 +0530

    added delete functionality and splay function

--- src/org/hsqldb/index/IndexSPlayMemory.java
+++ src/org/hsqldb/index/IndexSPlayMemory.java
@@ -54,66 +54,54 @@ public class IndexSPlayMemory extends IndexAVL{
             return;
         }
 
-//        while (true) {
+        Row currentRow = n.row;
+        compare = 0;
 
-            Row currentRow = n.row;
-            compare = 0;
+        if (compareSimple) {
+            /**
+             * compare = 0 -----> if rowData == currentRow
+             * compare = 1 -----> if currentRow == null
+             *                       rowData > currentRow
+             * compare = -1 ----> if rowData == null
+             *                       rowData < currentRow
+             */
+            compare =
+                    colTypes[0].compare(session, rowData[colIndex[0]],
+                            currentRow.getData()[colIndex[0]]);
 
-            if (compareSimple) {
-                /**
-                 * compare = 0 -----> if rowData == currentRow
-                 * compare = 1 -----> if currentRow == null
-                 *                       rowData > currentRow
-                 * compare = -1 ----> if rowData == null
-                 *                       rowData < currentRow
-                 */
-                compare =
-                        colTypes[0].compare(session, rowData[colIndex[0]],
-                                currentRow.getData()[colIndex[0]]);
-
-                if (compare == 0 && compareRowId) {
-                    compare = compareRowForInsertOrDelete(session, row,
-                            currentRow,
-                            compareRowId, 1);
-                }
-            } else {
+            if (compare == 0 && compareRowId) {
                 compare = compareRowForInsertOrDelete(session, row,
                         currentRow,
-                        compareRowId, 0);
+                        compareRowId, 1);
             }
+        } else {
+            compare = compareRowForInsertOrDelete(session, row,
+                    currentRow,
+                    compareRowId, 0);
+        }
 
-            // after the first match and check, all compares are with row id
-            if (compare == 0 && session != null && !compareRowId
-                    && session.database.txManager.isMVRows()) {
-                if (!isEqualReadable(session, store, n)) {
-                    compareRowId = true;
-                    compare = compareRowForInsertOrDelete(session, row,
-                            currentRow,
-                            compareRowId,
-                            colIndex.length);
-                }
+        // after the first match and check, all compares are with row id
+        if (compare == 0 && session != null && !compareRowId
+                && session.database.txManager.isMVRows()) {
+            if (!isEqualReadable(session, store, n)) {
+                compareRowId = true;
+                compare = compareRowForInsertOrDelete(session, row,
+                        currentRow,
+                        compareRowId,
+                        colIndex.length);
             }
+        }
 
-            if (compare == 0) {
-                if (isConstraint) {
-                    Constraint c =
-                            ((Table) table).getUniqueConstraintForIndex(this);
+        if (compare == 0) {
+            if (isConstraint) {
+                Constraint c =
+                        ((Table) table).getUniqueConstraintForIndex(this);
 
-                    throw c.getException(row.getData());
-                } else {
-                    throw Error.error(ErrorCode.X_23505, name.statementName);
-                }
+                throw c.getException(row.getData());
+            } else {
+                throw Error.error(ErrorCode.X_23505, name.statementName);
             }
-
-//            isleft = compare < 0;
-//            x      = n;
-//            n      = isleft ? x.nLeft
-//                    : x.nRight;
-//
-//            if (n == null) {
-//                break;
-//            }
-//        }
+        }
 
         if (compare < 0){
             NodeAVL newNode = ((RowAVL) row).getNode(position);
@@ -129,23 +117,181 @@ public class IndexSPlayMemory extends IndexAVL{
             root.nRight = null;
             store.setAccessor(this, newNode);
         }
-//        root = root.set(store, isleft, ((RowAVL) row).getNode(position));
-//
-//        balance(store, x, isleft);
-
         // CASE WHERE compare = 0
     }
 
 
-    void delete(PersistentStore store, NodeAVL x) {
+    public void delete(Session session, PersistentStore store, Row row) {
 
-        if (x == null) {
+        NodeAVL keyNode = ((RowAVL)row).getNode(position);
+        if ( keyNode == null) {
             return;
         }
+
         NodeAVL root = getAccessor(store);
 
+        root = splay(session,store,root, keyNode);
 
+        int cmp = this.compare(session,store,root,keyNode);
+
+        if (cmp == 0) {
+            if (root.nLeft == null) {
+                root = root.nRight;
+            }
+            else {
+                NodeAVL x = root.nRight;
+                root = root.nLeft;
+                splay(session,store,root,keyNode);
+                root.nRight = x;
+            }
+        }
     }
+
+    /***************************************************************************
+     * Splay tree function.
+     * **********************************************************************/
+    // splay key in the tree rooted at Node h. If a node with that key exists,
+    //   it is splayed to the root of the tree. If it does not, the last node
+    //   along the search path for the key is splayed to the root.
+
+    private NodeAVL splay(Session session,PersistentStore store,NodeAVL h, NodeAVL key) {
+
+        boolean        compareSimple = isSimple;
+        int            compare       = -1;
+        final Object[] rowData       = key.row.getData();
+        boolean        compareRowId  = !isUnique || hasNulls(session, rowData);
+
+        if (h == null) return null;
+
+        int compare1 = this.compare(session,store,h,key);
+
+        if (compare1 < 0) {
+            // key not in tree, so we're done
+            if (h.nLeft == null) {
+                return h;
+            }
+            int cmp2 = this.compare(session,store,h.nLeft,key);
+            if (cmp2 < 0) {
+                h.nLeft.nLeft= splay(session,store,h.nLeft.nLeft, key);
+                h = rotateRight(h);
+            }
+            else if (cmp2 > 0) {
+                h.nLeft.nRight = splay(session,store,h.nLeft.nRight, key);
+                if (h.nLeft.nRight!= null)
+                    h.nLeft = rotateLeft(h.nLeft);
+            }
+
+            if (h.nLeft == null) return h;
+            else                return rotateRight(h);
+        }
+
+        else if (compare1 > 0) {
+            // key not in tree, so we're done
+            if (h.nRight == null) {
+                return h;
+            }
+
+            int cmp2 = this.compare(session,store,h.nRight,key);
+            if (cmp2 < 0) {
+                h.nRight.nLeft= splay(session,store,h.nRight.nLeft, key);
+                if (h.nRight.nLeft!= null)
+                    h.nRight = rotateRight(h.nRight);
+            }
+            else if (cmp2 > 0) {
+                h.nRight.nRight= splay(session,store,h.nRight.nRight, key);
+                h = rotateLeft(h);
+            }
+
+            if (h.nRight == null) return h;
+            else                 return rotateLeft(h);
+        }
+
+        else return h;
+    }
+
+    private int compare(Session session,PersistentStore store,NodeAVL h, NodeAVL key){
+        boolean        compareSimple = isSimple;
+        int            compare       = -1;
+        final Object[] rowData       = key.row.getData();
+        boolean        compareRowId  = !isUnique || hasNulls(session, rowData);
+
+        if (compareSimple) {
+            /**
+             * compare = 0 -----> if rowData == currentRow
+             * compare = 1 -----> if currentRow == null
+             *                       rowData > currentRow
+             * compare = -1 ----> if rowData == null
+             *                       rowData < currentRow
+             */
+
+            compare =
+                    colTypes[0].compare(session, key.row.getData()[colIndex[0]],
+                            h.row.getData()[colIndex[0]]);
+
+            if (compare == 0 && compareRowId) {
+                compare = compareRowForInsertOrDelete(session, key.row,
+                        h.row,
+                        compareRowId, 1);
+            }
+        } else {
+            compare = compareRowForInsertOrDelete(session, key.row,
+                    h.row,
+                    compareRowId, 0);
+        }
+
+        // after the first match and check, all compares are with row id
+        if (compare == 0 && session != null && !compareRowId
+                && session.database.txManager.isMVRows()) {
+            if (!isEqualReadable(session, store, h)) {
+                compareRowId = true;
+                compare = compareRowForInsertOrDelete(session, key.row,
+                        h.row,
+                        compareRowId,
+                        colIndex.length);
+            }
+        }
+
+        return compare;
+    }
+
+    // right rotate
+    private NodeAVL rotateRight(NodeAVL h) {
+        NodeAVL x = h.nLeft;
+        h.nLeft = x.nRight;
+        x.nRight = h;
+        return x;
+    }
+
+    // left rotate
+    private NodeAVL rotateLeft(NodeAVL h) {
+        NodeAVL x = h.nRight;
+        h.nRight = x.nLeft;
+        x.nLeft = h;
+        return x;
+    }
+
+    /***************************************************************************
+     *  Helper functions.
+     ***************************************************************************/
+
+    // height of tree (1-node tree has height 0)
+//    public int height() { return height(root); }
+//    private int height(NodeAVL x) {
+//        if (x == null) return -1;
+//        return 1 + Math.max(height(x.nLeft), height(x.nRight));
+//    }
+//
+//
+//    public int size() {
+//        return size(root);
+//    }
+//
+//    private int size(NodeAVL x) {
+//        if (x == null) return 0;
+//        else return 1 + size(x.nLeft) + size(x.nRight);
+//    }
+
+
 
 
 }
